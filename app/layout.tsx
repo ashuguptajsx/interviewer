4430c363aae171d785d2f0ed3f534625acd208cb
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2025-03-22 22:50:52 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const monaSans = Mona_Sans({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
