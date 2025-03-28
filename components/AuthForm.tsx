fa6ef69ba970167f445484d348833ee46cb09f6f
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2025-03-28 22:50:22 +0530

    commit

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import Image from "next/image";
 
-const AuthForm = () => {
+
+
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+
+
+const AuthForm = ({type}:{type:FormType}) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  const isSignIn = type === "sign-in";
+
   return (
-    <div>AuthForm</div>
-  )
-}
+    <div className="card-border lg:min-w-[566px]">
+     <Form {...form}>
+  <div className="flex flex-col gap-6 card py-14 px-10">
+    <div className="flex flex-row gap-2 justify-center">
+      <Image src="/logo.svg" alt="logo" height={32} width={32} />
+      <h2 className="text-primary-100">Interviewer</h2>
+    </div>
+    <h3>Practice the job interviews with AI</h3>
 
-export default AuthForm
+    {/* Ensure form is properly structured */}
+    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-6 mt-4 form">
+      {!isSignIn && <p>Name</p>}
+      <Button type="submit">Submit</Button>
+    </form>
+  </div>
+</Form>
+
+    </div>
+    
+  );
+};
+
+export default AuthForm;
---
+++ components/FormField.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { FormItem, FormLabel, FormControl, FormDescription, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+
+const FormField = () => {
+  return (
+    <div>
+        <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  )
+}
+
+export default FormField
